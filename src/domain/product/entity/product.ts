43c55792dd66c58e2fe55d9de762c6513bb5f34c
Lucas Castro <lucas.castro1993@hotmail.com>
2024-04-30 10:23:02 -0300

    :bug: fix: refactor notitification test

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -42,6 +42,23 @@ export default class Product extends Entity implements ProductInterface {
   }
 
   validate() {
-    ProductYupValidator.create().validate(this);
+    if (this._id.length === 0) {
+      this.notification.addError({
+        context: "product",
+        message: "Id is required"
+      });
+    }
+    if (this._name.length === 0) {
+      this.notification.addError({
+        context: "product",
+        message: "Name is required"
+      });
+    }
+    if (this._price < 0) {
+      this.notification.addError({
+        context: "product",
+        message: "Price must be greater than zero"
+      });
+    }
   }
 }
